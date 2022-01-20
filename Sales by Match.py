c74e15525a59b8cebe8f43928b928d196791ad70
theCreator19 <77932869+theCreator19@users.noreply.github.com>
2022-01-20 22:15:09 +0530

    Create Sales by Match.py

---
+++ Sales by Match.py
@@ -0,0 +1,42 @@
+# Sales by Match
+# HackerRank Problem Solved using Python.
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    # Write your code here
+    sum=0
+    a=set(ar)
+    for i in a:
+        c= ar.count(i)
+        t=c//2
+        sum+=t
+    return sum
+        
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+    

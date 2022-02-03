4678a3a4eb5b9d07c6931907e9796409159e3670
Aman Agarwal <77932869+theCreator19@users.noreply.github.com>
2022-02-04 00:00:04 +0530

    Create Sherlock and Squares.py

---
+++ Sherlock and Squares.py
@@ -0,0 +1,48 @@
+# Sherlock and Squares
+
+# HackerRank Problem Solved using Python.
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'squares' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER a
+#  2. INTEGER b
+#
+
+def squares(a, b):
+    # Write your code here
+    s1=math.sqrt(a)
+    s2=int(math.sqrt(b))
+    if (s1!=int(s1)):
+        t1=int(s1)+1
+    else:
+        t1=int(s1)
+    return s2-t1+1    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        first_multiple_input = input().rstrip().split()
+
+        a = int(first_multiple_input[0])
+
+        b = int(first_multiple_input[1])
+
+        result = squares(a, b)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
